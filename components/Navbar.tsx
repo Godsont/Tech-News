0205424ff6928bd82d8aa097c326b8f9f1545e68
Godson Thomas <godson@Godsons-MacBook.local>
2023-10-05 02:06:10 +0530

    fixed navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -37,7 +37,7 @@ export default function Navbar() {
           </h1>
         </Link>
         <p className="text-sm">
-          Exploring Tomorrow's Innovations, <br /> One Byte at a Time.
+          Exploring Tomorrow&apos;s Innovations, <br /> One Byte at a Time.
         </p>
       </div>
 
